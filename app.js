30260007bb178cb1f99a8b7ba1dfebee1e630f6a
sancho <sancho@yopmail.com>
2017-08-01 15:41:14 +0300

    Bot

---
+++ app.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const TelegramNodeBot = require('telegram-node-bot')
+const telegram = new TelegramNodeBot.Telegram(process.env.TELEGRAM_TOKEN)
+const source = require('quotes.json')
+
+class QuotesController extends TelegramNodeBot.TelegramBaseController {
+    /**
+     * @param {Scope} $
+     */
+    quoteCommandHandler($) {
+      var quotes = source.quotes
+      var randomQuote = quotes[Math.floor(Math.random()*quotes.length)]
+      $.sendMessage(randomQuote.text)
+    }
+
+    get routes() {
+      return {
+        'quoteCommand': 'quoteCommandHandler'
+      }
+    }
+}
+
+telegram.router.when(new TelegramNodeBot.TextCommand('quote', 'quoteCommand'), new QuotesController())
